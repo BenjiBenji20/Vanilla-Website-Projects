5d136a6d7a5fa242fbca52d0accd455f237beeb8
Azathoth <129529261+BenjiBenji20@users.noreply.github.com>
2024-12-09 20:12:21 +0800

    Delete docs directory

--- docs/JS/contact.js
+++
@@ -1,9 +0,0 @@
-const paragraphElement = document.querySelector('.profession');
-
-setTimeout(() => {
-  paragraphElement.style.visibility = "visible";
-  paragraphElement.style.overflow = "hidden";
-  paragraphElement.style.whiteSpace = "nowrap";
-  paragraphElement.style.borderRight = "2px solid";
-  paragraphElement.style.animation = "typing 2s steps(22) forwards, blink 1s step-end infinite";
-}, 1000);
