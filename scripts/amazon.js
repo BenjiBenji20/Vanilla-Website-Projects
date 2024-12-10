c476035205d669abe3547ad2dea69f60ab769341
BenjiBenji20 <imperialbenjie393@gmai.com>
2024-12-10 20:51:46 +0800

    Used products.js file to transfer value.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,67 +1,12 @@
 // Save the data
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
-  }, 
-  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095
-  },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  },
-  {
-    image: 'images/products/black-2-slot-toaster.jpg',
-    name: '2 Slot Toaster - Black',
-    rating: {
-      stars: 5,
-      count: 2197
-    },
-    priceCents: 1899
-  },
-  {
-    image: 'images/products/6-piece-white-dinner-plate-set.jpg',
-    name: '6 Piece White Dinner Plate Set',
-    rating: {
-      stars: 4,
-      count: 37
-    },
-    priceCents: 2067
-  },
-  {
-    image: 'images/products/6-piece-non-stick-baking-set.webp',
-    name: '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set',
-    rating: {
-      stars: 4.5,
-      count: 175
-    },
-    priceCents: 3499
-  }
-];
+// cosnst products = []; products variable was loaded or passed from another js file from data/products.js 
 
 let productsHTML = '';
 // Loop through the array
 products.forEach((product) => {
   // accumalte 
   productsHTML += `
-    <div class="js-product-container product-container" data-product-id="e43638ce-6aa0-4b85-b27f-e1d07eb678c6" data-testid="product-container-e43638ce-6aa0-4b85-b27f-e1d07eb678c6">
+    <div class="js-product-container product-container" data-product-id="${product.id}" data-testid="product-container-${product.id}">
       <div class="product-image-container">
         <img class="js-product-image product-image" src="${product.image}">
       </div>
