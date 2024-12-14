8351bcfec9873b4499b68cfc1120ff70639139c1
BenjiBenji20 <imperialbenjie393@gmai.com>
2024-12-14 21:06:52 +0800

    Save delivery date data using external dayjs library and shipping fee then generate HTML for delivery options.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,8 @@
 import { cart, removeCartItem, calculateCartQuantity, updateQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; // library to set delivery schedule on current date
+import { deliveryOptions } from '../data/deliveryOptions.js';
 
-var now = Date.now();
-
-const deliveryDate = dayjs().add(7, 'days').format('dddd, MMMM DD');
 
 let orderSummary = '';
 cart.forEach((cartItems) => {
@@ -15,6 +14,22 @@ cart.forEach((cartItems) => {
       matchingProduct = prod;
     }
   });
+
+  // extracting delivery date that matched to the selected delivery option
+  const deliveryOptionID = cartItems.deliveryOptionID;
+
+  let deliveryOption;
+
+  // loop to extract matching delivery option
+  deliveryOptions.forEach((option) => {
+    if(option.id === deliveryOptionID) {
+      deliveryOption = option;
+    }
+  });
+
+  const deliveryDate = dayjs()
+      .add(deliveryOption.deliveryDays,'days')
+      .format('dddd, MMMM DD');
 
   orderSummary += 
     `
@@ -56,48 +71,51 @@ cart.forEach((cartItems) => {
               Choose a delivery option:
             </div>
 
-            <div class="delivery-option">
-              <input type="radio" class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-              <div>
-                <div class="delivery-option-date">
-                  Tuesday, June 21
-                </div>
-                <div class="delivery-option-price">
-                  FREE Shipping
-                </div>
-              </div>
-            </div>
-            <div class="delivery-option">
-              <input type="radio" checked class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-              <div>
-                <div class="delivery-option-date">
-                  Wednesday, June 15
-                </div>
-                <div class="delivery-option-price">
-                  $4.99 - Shipping
-                </div>
-              </div>
-            </div>
-            <div class="delivery-option">
-              <input type="radio" class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-              <div>
-                <div class="delivery-option-date">
-                  Monday, June 13
-                </div>
-                <div class="delivery-option-price">
-                  $9.99 - Shipping
-                </div>
-              </div>
-            </div>
+            ${deliveryOptionRenderer(matchingProduct, cartItems)}
+
           </div>
         </div>
       </div>
     `
 }); 
 
+
+// Rendering delivery options to match the current date
+function deliveryOptionRenderer(matchingProduct, cartItems) {
+  let htmlElements = '';
+
+  deliveryOptions.forEach((option) => {
+    const deliveryDate = dayjs()
+      .add(option.deliveryDays,'days')
+      .format('dddd, MMMM DD');
+    
+    const priceString = option.shippingFee === 0 ? 
+      'FREE' : `$${(option.shippingFee / 100).toFixed(2)} -`;
+
+    const ischecked = option.id === cartItems.deliveryOptionID;
+
+    htmlElements += ` 
+      <div class="delivery-option">
+        <input type="radio" class="delivery-option-input"
+          ${ischecked ? 'checked' : ''}
+          name="delivery-option-${matchingProduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            ${deliveryDate}
+          </div>
+          <div class="delivery-option-price">
+            ${priceString} Shipping
+          </div>
+        </div>
+      </div>  
+    `
+  });
+
+  // return the html elements to be rendered
+  return htmlElements;
+}
+
+// Render the html elements
 document.querySelector('.order-summary').innerHTML = orderSummary;
 
 // change checkout quantity on header
