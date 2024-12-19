eaff645c1fb1ebbb21130c86d8fa25bb633acb29
BenjiBenji20 <imperialbenjie393@gmai.com>
2024-12-19 20:50:08 +0800

    Learn promises nd convert from call back to a promise.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,11 +3,26 @@ import { renderPaymentSummary } from "./checkout/payment-summary.js";
 import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js';
 
-// pass the two functions to render
-loadProducts(() => {
-// render the checkout summary
-renderOrderSummary();
+// promise class to handle asynchronouse code
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  })
+}).then(() => {
+    // render the checkout summary
+    renderOrderSummary();
+      
+    // render the payment summary
+    renderPaymentSummary();
+  });
 
-// render the payment summary
-renderPaymentSummary();
+/*
+// callback
+loadProducts(() => {
+  // render the checkout summary
+  renderOrderSummary();
+      
+  // render the payment summary
+  renderPaymentSummary();
 });
+*/
